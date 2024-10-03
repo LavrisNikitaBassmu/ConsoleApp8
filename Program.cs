b9e6d090942636cdf3568486a5a309388ce34b0d
Admin <Admin@WIN-0QFN66U68EP>
2024-10-03 18:14:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class ReverseNumber
+{
+    static void Main(string[] args)
+    {
+        // Запрашиваем у пользователя трехзначное число
+        Console.Write("Введите трехзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Вызываем метод Reverse с полученным значением
+        int reversed = Reverse(number);
+
+        // Выводим результат на консоль
+        Console.WriteLine($"Обратное число: {reversed}");
+    }
+
+    static int Reverse(int number)
+    {
+        // Инициализируем переменную для хранения обратного числа
+        int reversed = 0;
+
+        // Разбираем число на цифры и переставляем их в обратном порядке
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        // Возвращаем обратное число
+        return reversed;
+    }
+}
